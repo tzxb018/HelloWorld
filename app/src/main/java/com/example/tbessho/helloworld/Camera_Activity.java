fc159df740ff944f4745f00354457f4d95312a67
tzxb018 <tzxbapp018@gmail.com>
2017-06-07 10:50:56 -0500

    Updated Camera

--- app/src/main/java/com/example/tbessho/helloworld/Camera_Activity.java
+++ app/src/main/java/com/example/tbessho/helloworld/Camera_Activity.java
@@ -1,25 +1,16 @@
 package com.example.tbessho.helloworld;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import static android.content.ContentValues.TAG;
 
 /**
  * Created by tbessho on 6/7/2017.
@@ -27,127 +18,44 @@ import static android.content.ContentValues.TAG;
 
 public class Camera_Activity extends Activity{
 
-    private Camera mCamera;
-    private CameraPreview mPreview;
-
+    Button button;
+    ImageView imageView;
+    static final int CAM_REQUEST = 1;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_activity);
+        button = (Button) findViewById(R.id.button_capture);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
-        //creating the camera
-        mCamera = getCameraInstance();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
 
-        //create preview view
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
-
-        // Add a listener to the Capture button
-        Button captureButton = (Button) findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // get an image from the camera
-                        mCamera.takePicture(null, null, mPicture);
-                    }
-                }
-        );
-    }
-
-    //detection of a camera
-    private boolean checkCameraHardware (Context context){
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            return true; // case where the device has a camera
-        }
-        else{
-            return false;
-        }
-    }
-
-    //accessing the camera
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); //attempt to get a camera instance
-        }
-        catch (Exception e){
-            //Camera is not available (in use or does not exist)
-        }
-
-        return c;
-    }
-
-    //taking the picture
-    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
-        File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-            try {
-                FileOutputStream fos = new FileOutputStream(pictureFile);
-                fos.write(data);
-                fos.close();
-            } catch (FileNotFoundException e) {
-                Log.d(TAG, "File not found: " + e.getMessage());
-            } catch (IOException e) {
-                Log.d(TAG, "Error accessing file: " + e.getMessage());
+                startActivityForResult(cameraIntent, CAM_REQUEST);
             }
+
+        });
+    }
+
+    private File getFile(){
+        File folder = new File ("sdcard/camera_app");
+        if (!folder.exists()){
+            folder.mkdir();
         }
-    };
+
+        File image_file = new File(folder, "cam_image.jpg");
+        return image_file;
+    }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        releaseCamera();              // release the camera immediately on pause event
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        String path = "sdcard/camera_app/cam_image.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
     }
-
-    private void releaseCamera(){
-        if (mCamera != null){
-            mCamera.release();        // release the camera for other applications
-            mCamera = null;
-        }
-    }
-
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
-
-    /** Create a file Uri for saving an image or video */
-    private static Uri getOutputMediaFileUri(int type){
-        return Uri.fromFile(getOutputMediaFile(type));
-    }
-
-    /** Create a File for saving an image or video */
-    private static File getOutputMediaFile(int type){
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
-                Log.d("MyCameraApp", "failed to create directory");
-                return null;
-            }
-        }
-
-        // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE){
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_"+ timeStamp + ".jpg");
-        } else {
-            return null;
-        }
-
-        return mediaFile;
-    }
-
-
 }
 
